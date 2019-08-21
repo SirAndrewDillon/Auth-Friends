07348b13b97b1d3e218eec90647ce82aa2e402e6
andrew dillon <majik895@gmail.com>
2019-08-21 10:16:19 -0400

    fixed appjs

--- sidekick/src/App.js
+++ sidekick/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { logout } from './actions';
+
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
+import PrivateRoute from './components/PrivateRoute';
+
+import './App.scss';
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          {this.props.error !== '' && (<div className="error-text">{this.props.error}</div>)}
+          <header>
+            <h1>ReduxFriends</h1>
+            <nav>
+              {this.props.loggedIn && (
+                <>
+                  <Link to="/my-friends">myFriends</Link>
+                  <Link onClick={this.props.logout} to="/">Logout</Link>
+                </>
+              )}
+            </nav>
+          </header>
+          <Route exact path="/" component={Login} />
+          {this.props.loggedIn && (<PrivateRoute path="/my-friends" component={FriendsList} />)}
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+const mstp = state => ({
+  error: state.error,
+  loggedIn: state.loggedIn,
+});
+
+
+export default connect(mstp, { logout })(App);

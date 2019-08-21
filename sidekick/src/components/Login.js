c6b0964c6b01378c26d57b9d21e23dc039579892
andrew dillon <majik895@gmail.com>
2019-08-21 10:08:14 -0400

    friend card

---
+++ sidekick/src/components/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { deleteFriend } from '../actions';
+
+import './FriendCard.scss';
+
+const FriendCard = props => {
+    return (
+        <div className="FriendCard" key={props.id}>
+            <h3>{props.name}</h3>
+            <p>{props.age} years old</p>
+            <a href={`${props.email}`}>{props.email}</a>
+            <button onClick={props.deleteFriend} value={props.id}>Delete</button>
+        </div>
+    );
+}
+
+export default connect(null, { deleteFriend })(FriendCard);
